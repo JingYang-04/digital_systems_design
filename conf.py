d346341ce840db40e2a6a0620b694c9b178b8d93
bjones1 <bjones@ece.msstate.edu>
2022-11-30 14:59:20 -0600

    Clean: Fix typo.

--- conf.py
+++ conf.py
@@ -238,7 +238,7 @@ html_static_path = runestone_static_dirs() + [
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Digitial Systems Design'
+html_title = 'Digital Systems Design'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_short_title = 'DSD'
